79d1b184d935e92bdbc572956c9845e2647043f5
Terkwood <metaterkhorn@gmail.com>
2018-06-14 11:03:32 -0900

    close enough

--- gpio_light_up.sh
+++ gpio_light_up.sh
@@ -2,7 +2,7 @@
 
 for arg in "$@"
 do
-    echo $arg > /sys/class/gpio/export \ 
-      && echo out > /sys/class/gpio/gpio$arg/direction \
-      && echo 1   > /sys/class/gpio/gpio$arg/value 
+    echo $arg > /sys/class/gpio/export 
+    echo out > /sys/class/gpio/gpio$arg/direction 
+    echo 1   > /sys/class/gpio/gpio$arg/value 
 done
