293c1f97e84601bcb7bbf8116aeb5983a2a19633
Terkwood <metaterkhorn@gmail.com>
2018-06-14 15:52:22 -0400

    add script

--- gpio_export.sh
+++ gpio_export.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-# TODO check args
+# USAGE:
+#    gpio_export.sh 5
+#    gpio_export.sh 5 17 26
+#
+#    Exports the given GPIO pins so that they can be 
+#    handled by the user
+
+for arg in "$@"
+do
+    echo $arg > /sys/class/gpio/export
+done
